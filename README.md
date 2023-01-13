ddab96b1d205a41a1226e8e7ae4925c7f34b2930
Benedikt <mail@benediktbast.com>
2023-01-13 18:10:45 +0100

    check for file parameters and add example file

--- README.md
+++ README.md
@@ -5,10 +5,17 @@ Requires MAC OS say command, sox, and ffmpeg if you want to convert the output t
 ## Usage
 Arguments:
 - name of csv file
+- name of voice 1
+- name of voice 2
 - line number to start
 - line number to stop
+- output format (optional) can be set to `mp3` if you have ffmpeg installed
 
-`./csv2speech.sh example.csv 1 5`
+You can use the example CSV file with German and Thai Translation to convert lines 1 to 7
+`./csv2speech.sh example_de_th.csv Anna Kanya 1 7`
+
+with conversion to mp3
+`./csv2speech.sh example_de_th.csv Anna Kanya 1 7 mp3`  
 
 ## Find the right voice
 The command `say -v '?'` will list you all available voices on your system
--- csv2speech.sh
+++ csv2speech.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-IFS_DEFAULT=$IFS    #store default seperator
+
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+DEFAULT_COLOR=$'\e[0m'
+
+if [[ -z "$1" ]]
+then
+    echo "${RED}No input file given$DEFAULT_COLOR" 
+    exit 2
+fi 
+
+if [[ ! -f "$1" ]]
+then
+    echo "${RED}File does not exist$DEFAULT_COLOR"
+    exit 2
+fi
+
 INPUT_FILE=$1       #arg1 =  path to csv file
 INPUT_FILE_TRIMMED=$(echo "$INPUT_FILE" | cut -f 1 -d '.')
-START=$2            #line to start
-END=$3              #line to end
+
+VOICE_1=$2
+VOICE_2=$3
+
+START=$4            #line to start
+END=$5              #line to end
+
+OUTPUT_FORMAT=$6
+
 FILECOUNT=1
 COUNTER=$START      #counter caounts from start value
+
+IFS_DEFAULT=$IFS    #store default seperator
 IFS=","             #use comma as seperator
+
 TMP_FOLDER="tmp-${INPUT_FILE_TRIMMED}-${START}-${END}"
 TMP_FILE="input-${START}-${END}.csv"   #temp csv file for parsing
 
@@ -20,9 +46,9 @@ awk -F, 'NR=='$START' ,NR=='$END' { print $1","$2}' $INPUT_FILE>"${TMP_FOLDER}/$
 
 while read col1 col2    #read both columns from temp file
 do
-    say -v Anna $COUNTER"[[ slnc 1000 ]]" $col1 "[[ slnc 1000 ]]" -o "${TMP_FOLDER}/$(printf %05d $FILECOUNT).${COUNTER}.en.aiff"
+    say -v Anna $COUNTER"[[ slnc 1000 ]]" $col1 "[[ slnc 1000 ]]" -o "${TMP_FOLDER}/$(printf %05d $FILECOUNT).${COUNTER}.lang1.aiff"
     let FILECOUNT+=1
-    say -v Kanya $col2 "[[ slnc 7000 ]] "  -o "${TMP_FOLDER}/$(printf %05d $FILECOUNT).${COUNTER}.th.aiff"
+    say -v Kanya $col2 "[[ slnc 7000 ]] "  -o "${TMP_FOLDER}/$(printf %05d $FILECOUNT).${COUNTER}.lang2.aiff"
     let FILECOUNT+=1
     let COUNTER+=1
 done< "${TMP_FOLDER}/${TMP_FILE}"
@@ -30,4 +56,8 @@ done< "${TMP_FOLDER}/${TMP_FILE}"
 IFS=$IFS_DEFAULT
 
 sox $(ls ${TMP_FOLDER}/*.aiff | sort -n) "${INPUT_FILE_TRIMMED}-${START}-${END}.wav"
-ffmpeg -i "${INPUT_FILE_TRIMMED}-${START}-${END}.wav" "${INPUT_FILE_TRIMMED}-${START}-${END}.mp3"
+
+if [[ "$6" = "mp3" ]]
+then 
+    ffmpeg -i "${INPUT_FILE_TRIMMED}-${START}-${END}.wav" "${INPUT_FILE_TRIMMED}-${START}-${END}.mp3"
+fi
